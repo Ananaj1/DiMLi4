88b2d9786d902858375242a8c848c4538c1fa5d2
Ananaj1 <128913118+Ananaj1@users.noreply.github.com>
2023-05-12 18:58:23 +0300

    two numbers, addition and multiplication

---
+++ projekt2.cpp
@@ -0,0 +1,16 @@
+﻿// projekt2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL,"RU");
+    std::cout << "введите 2 числа"<<std::endl;
+    int v = 0,v2 = 0;
+    std::cin >> v >> v2;
+    std::cout << "При сложении "<<v<<" И "<<v2<<" получим "<<v+v2<<std::endl <<" A при умножении получим "<<v*v2;
+    return 0;
+}
+
+
